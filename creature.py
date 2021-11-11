4f1f383e6be9fa41874a5bc79e451ec008a7c594
Yufei <yutao@pdx.edu>
2021-11-11 07:35:36 -0800

    fixed formatting in creature.py

--- creature.py
+++ creature.py
@@ -1,7 +1,8 @@
-#Joint effort by: Jerry Smedley, Jacob Hilt, Melissa Barnes, Mark Montes, Anthony Chin
-#This work is made available under the "MIT License". Please see the file LICENSE in this distribution for license terms.
+# Joint effort by: Jerry Smedley, Jacob Hilt, Melissa Barnes, Mark Montes, Anthony Chin
+# This work is made available under the "MIT License". Please see the file LICENSE in this
+# distribution for license terms.
 
-#type = 0, dead; type = 1, stationary; type = 2, move along x pos; type = 3, move along y pos
+# type = 0, dead; type = 1, stationary; type = 2, move along x pos; type = 3, move along y pos
 class creature:
     typ = 0
     xpos = 0
@@ -13,58 +14,58 @@ class creature:
         typ = 1
 
     def movement(self, mapObj):
-        if(self.alive == 0):
-            #mapObj.objArr[self.ypos][self.xpos] = '-'
+        if self.alive == 0:
+            # mapObj.objArr[self.ypos][self.xpos] = '-'
             return [0, mapObj]
-        if(self.typ == 0 or self.typ == 1):
+        if self.typ == 0 or self.typ == 1:
             return [1, mapObj]
-        
-        #moves along the x pos
-        if(self.typ == 2):
+
+        # moves along the x pos
+        if self.typ == 2:
             desx = 0
-            if(self.direction == 1):
+            if self.direction == 1:
                 desx = self.xpos + 1
             else:
                 desx = self.xpos - 1
-            
-            if(desx >= mapObj.maxX or desx < 0):
+
+            if desx >= mapObj.maxX or desx < 0:
                 self.direction = self.direction * -1
                 return [0, mapObj]
 
-            if(mapObj.objArr[self.ypos][desx] == '-'):
+            if mapObj.objArr[self.ypos][desx] == '-':
                 mapObj.objArr[self.ypos][desx] = str(self.typ)
                 mapObj.objArr[self.ypos][self.xpos] = '-'
                 self.xpos = desx
                 return [1, mapObj]
 
             # add a kill player here
-            if(mapObj.objArr[self.ypos][desx] == 'p'):
+            if mapObj.objArr[self.ypos][desx] == 'p':
                 return [2, mapObj]
 
             else:
                 self.direction = self.direction * -1
                 return [0, mapObj]
 
-        #moves along the y pos
-        if(self.typ == 3):
+        # moves along the y pos
+        if self.typ == 3:
             desy = 0
-            if(self.direction == 1):
+            if self.direction == 1:
                 desy = self.ypos + 1
             else:
                 desy = self.ypos - 1
-            
-            if(desy >= mapObj.maxY or desy < 0):
-                self.direction = self.direction * -1
-                return [0, mapObj] 
 
-            if(mapObj.objArr[desy][self.xpos] == '-'):
+            if desy >= mapObj.maxY or desy < 0:
+                self.direction = self.direction * -1
+                return [0, mapObj]
+
+            if mapObj.objArr[desy][self.xpos] == '-':
                 mapObj.objArr[desy][self.xpos] = str(self.typ)
                 mapObj.objArr[self.ypos][self.xpos] = '-'
                 self.ypos = desy
                 return [1, mapObj]
 
             # add a kill player here
-            if(mapObj.objArr[desy][self.xpos] == 'p'):
+            if mapObj.objArr[desy][self.xpos] == 'p':
                 return [2, mapObj]
 
             else:
@@ -73,10 +74,9 @@ class creature:
 
         return [1, mapObj]
 
-        
     def dead(self):
         self.alive = 0
         return 1
-    
+
     def status(self):
         return self.alive
